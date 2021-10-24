b81bbefd62881de14ad109c139b829608bfabb27
Paul K <laupski@users.noreply.github.com>
2021-10-24 14:07:56 -0700

    add stock commands (#3)

---
+++ commands/price.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-unresolved */
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const config = require('../config/config.json');
+// eslint-disable-next-line import/order
+const alpha = require('alphavantage')({ key: config.alphavantage });
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('price')
+    .setDescription('Get price of a ticker')
+    .addStringOption((option) => option.setName('ticker').setDescription('The ticker to get a quote')),
+  async execute(interaction) {
+    const ticker = interaction.options.getString('ticker');
+    if (ticker) {
+      alpha.data.quote(ticker).then((data) => {
+        console.log(data);
+        const price = data['Global Quote']['05. price'];
+        return interaction.reply(`Current price of \`${ticker}\`: ${price}`);
+      }).catch((error) => {
+        console.error(error);
+        return interaction.reply('Error has occurred');
+      });
+    } else {
+      return interaction.reply('No ticker has been chosen');
+    }
+    return null;
+  },
+};

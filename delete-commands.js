41f2d46c21d2db08d890ed0177e864315dfaf6fd
laupski <pkawalkowski@gmail.com>
2021-10-23 21:57:16 -0700

    fix linting

--- delete-commands.js
+++ delete-commands.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config/config.json');

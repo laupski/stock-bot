9b2dc1e5d48d395b28854c6cb5376320a86329d2
Mack Stump <mack@mackstump.com>
2021-10-26 09:42:06 -0400

    refactor: use async/await and add error handling to delete-commands

---
+++ src/cmd/create-commands.ts
@@ -0,0 +1,24 @@
+import * as fs from "fs";
+import {REST} from "@discordjs/rest";
+import {Routes} from "discord-api-types/v9";
+import {Config, EnvironmentConfig} from "../config";
+
+async function main(config: Config) {
+    const commands: string[] = [];
+    const commandFiles = fs.readdirSync(`${__dirname}/../commands`).filter((file) => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`${__dirname}/../commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
+
+    const rest = new REST({version: '9'}).setToken(config.botToken);
+
+    rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), {body: commands})
+        .then(() => console.log('Successfully registered application commands.'))
+        .catch(console.error);
+}
+
+(async () => {
+    await main(new EnvironmentConfig());
+})();

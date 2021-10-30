0e8e49d9781cd85f414c3f4297038c0858c19cd2
laupski <pkawalkowski@gmail.com>
2021-10-29 17:34:28 -0700

    feat: add live price built into /price command

--- src/commands/price.ts
+++ src/commands/price.ts
@@ -1,38 +1,24 @@
-import Alpha from 'alphavantage';
 import { Config } from '../config';
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 export = {
-  _alpha: undefined,
-
   data: new SlashCommandBuilder()
     .setName('price')
     .setDescription('Get price of a ticker')
     .addStringOption((option) => option.setName('ticker').setDescription('The ticker to get a quote')),
 
   async execute(interaction, config: Config) {
-    if (typeof this.alpha === 'undefined') {
-      this.alpha = Alpha({ key: config.alphaKey });
-    }
-    const { alpha } = this;
-
+      // eslint-disable-next-line global-require
+      const fmp = require('financialmodelingprep')(config.fmpKey)
     const ticker = interaction.options.getString('ticker');
     if (ticker) {
-      alpha.data
-        .quote(ticker)
-        .then((data) => {
-          console.log(data);
-          const price = data['Global Quote']['05. price'];
-          const percent = data['Global Quote']['10. change percent'];
-          const previousclose = data['Global Quote']['08. previous close'];
-          return interaction.reply(
-            `Current price of \`${ticker}\`: ${price} (${percent}) Previous close: ${previousclose}`
-          );
-        })
-        .catch((error) => {
-          console.error(error);
-          return interaction.reply('Error has occurred');
+        fmp.stock(ticker).quote().then(response => {
+            console.log(response);
+            return interaction.reply(`Current price of ${ticker}: $${response[0].price} (${response[0].changesPercentage}%), volume: ${response[0].volume}`);
+        }).catch((error) => {
+            console.log(error);
+            return interaction.reply('Error on fetching live price.')
         });
     } else {
       return interaction.reply('No ticker has been chosen');

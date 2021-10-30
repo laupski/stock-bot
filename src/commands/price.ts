5722a89d8b79ebaf5314bc3263e2a945038f4450
laupski <pkawalkowski@gmail.com>
2021-10-29 17:38:27 -0700

    style: linting

--- src/commands/price.ts
+++ src/commands/price.ts
@@ -9,16 +9,22 @@ export = {
     .addStringOption((option) => option.setName('ticker').setDescription('The ticker to get a quote')),
 
   async execute(interaction, config: Config) {
-      // eslint-disable-next-line global-require
-      const fmp = require('financialmodelingprep')(config.fmpKey)
+    // eslint-disable-next-line global-require
+    const fmp = require('financialmodelingprep')(config.fmpKey);
     const ticker = interaction.options.getString('ticker');
     if (ticker) {
-        fmp.stock(ticker).quote().then(response => {
-            console.log(response);
-            return interaction.reply(`Current price of ${ticker}: $${response[0].price} (${response[0].changesPercentage}%), volume: ${response[0].volume}`);
-        }).catch((error) => {
-            console.log(error);
-            return interaction.reply('Error on fetching live price.')
+      fmp
+        .stock(ticker)
+        .quote()
+        .then((response) => {
+          console.log(response);
+          return interaction.reply(
+            `Current price of ${ticker}: $${response[0].price} (${response[0].changesPercentage}%), volume: ${response[0].volume}`
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+          return interaction.reply('Error on fetching live price.');
         });
     } else {
       return interaction.reply('No ticker has been chosen');
